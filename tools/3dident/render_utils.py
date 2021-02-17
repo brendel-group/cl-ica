3f348c0dc196ed1fc9b9a285c5d8c543a662b082
zimmerrol <rzrolandzimmermann@gmail.com>
2021-02-18 00:53:12 +0100

    Update missing license information

--- tools/3dident/render_utils.py
+++ tools/3dident/render_utils.py
@@ -5,8 +5,8 @@
 # All rights reserved.
 #
 # This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
+# ORIGNAL_LICENSE file. An additional grant
+# of patent rights can be found in the ORIGINAL_PATENTS file in the same directory.
 
 import sys, random, os, json
 import bpy, bpy_extras
